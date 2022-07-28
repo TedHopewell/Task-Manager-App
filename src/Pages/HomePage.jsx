b6d6a7a56a8eb615d7f46b51fce701d52fae42a7
TedHopewell <hopewellmaome@gmail.com>
2022-07-28 15:40:08 +0200

    final update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,22 +1,106 @@
-import React from "react";
-import AddTask from "../Components/AddTask";
-import TaskList from "../Components/TaskList";
-import LoginPage from "../Pages/LoginPage";
-import SignInPage from "./SignInPage"; 
 
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
+  import '../css/HomePage.css';
+  
+  import {db} from '../Config-file/firebase';
+  
+  import {addDoc, collection,getDocs} from 'firebase/firestore';
+  
+ 
 
 function HomePage() {
+  const[task, setTask]= useState('');
+  const[priority, setPriority] = useState('');
 
- 
-  return (
-   <div>
-   
-    <AddTask />
-    <TaskList/>
-   </div>
-  )
-}
+  const[items, setItem] = useState([]);
+
+  const itemRef =collection(db,"Tasks");
+
+  const getItems = async()=>{
+      let data = await getDocs(itemRef);
+      setItem(data.docs.map((doc)=>({...doc.data(), id: doc.id})))
+  }
+
+  const addItems = async()=>{
+     await addDoc(itemRef, {task:task, priority:priority})
+     alert("Item added Successfully");
+    getItems();
+    }
+    
+
+    useEffect(()=>{
+      getItems();
+   }, [])
+
+
+  
+  
+  
+    return (
+      <div className="AddTask">
+          <div className="topPart">
+              <h1>Welcome Hopewell</h1>
+              <button>LogOut</button>
+          </div>
+  
+          <div className="bottomPart">
+            <div className="input-fields">
+            <input type="text" placeholder="Add New Task" onChange={(e)=>setTask(e.target.value)}/>
+              <select name="priority" id="priority" onChange={(e)=>setPriority(e.target.value)}>
+                  <option value="Priority">PRIORITY</option>
+                  <option value="High">High</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Low">Low</option>
+  
+              </select>
+              <button onClick={addItems}>+</button>
+            </div>
+
+            <div className="task-list">
+            {
+                        items.map(item=>((
+                            <div className="items" key={item.id}>
+                                {
+                                    item.priority == "Low" ? (
+                                        
+                                        <div className="item" style={{borderBottom: "3px solid green"}}>
+                                        <span>{item.task}</span>
+                                        <button>complete</button>
+                                        </div>
+                                
+                                    ):item.priority == "Medium" ? (
+                                        <div className="item" style={{borderBottom: "3px solid orange"}}>
+                                        <span>{item.task}</span>
+                                        <button >complete</button>
+                                        </div>
+                                    ):item.priority == "High" ? (
+                                        <div className="item" style={{borderBottom: "3px solid red"}}>
+                                        <span>{item.task}</span>
+                                        <button>complete</button>
+        
+                                        </div>
+                                    ):(
+                                        <div>
+
+                                        </div>
+                                    )
+                                }
+
+
+
+                                
+                            </div> 
+                        )))
+                    }
+            </div>
+              
+          </div>
+          
+      </div>
+    )
+  }
+  
+
 
 export default HomePage;
